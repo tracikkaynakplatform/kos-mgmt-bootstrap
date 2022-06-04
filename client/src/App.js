0a063aa059d287dd943611282a7669f33d769b17
Fatih Karatay <karatayy61@gmail.com>
2022-06-04 21:15:57 +0300

    add create cluster component and fix route

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,19 @@
-import Sidebar from "./components/Navbar";
-import {routes} from "./routes";
-import {Route, Routes} from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { routes } from "./routes";
+import { Route, Routes } from "react-router-dom";
 
 
 function App() {
     return (
         <div className="min-h-full">
-            <Sidebar/>
+            <Navbar />
             <main>
 
-                            <Routes>
-                                    {routes.map(route => (
-                                        <Route key={route.id} path={route.path} exact={route.exact} element={<route.component/>}/>
-                                    ))}
-                            </Routes>
+                <Routes>
+                    {routes.map(route => (
+                        <Route key={route.id} path={route.path} exact={route.exact} element={<route.component />} />
+                    ))}
+                </Routes>
 
             </main>
         </div>
